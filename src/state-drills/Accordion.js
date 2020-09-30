77ee4fe5ab78b5932a73cf77ff8d3928b39e194c
nickjang <nickjang@alumni.upenn.edu>
2020-09-30 14:37:50 -0400

    Add Accordian.js

---
+++ src/state-drills/Accordion.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+
+class Accordion extends Component {
+  defaultProps = {
+    sections: []
+  };
+
+  state= {
+
+  };
+
+  componentDidMount() {
+
+  }
+
+  componentDidUpdate() {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Accordion;
